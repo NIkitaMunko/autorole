35041e80858c8a4025a3d222415c9f2418c5e3e9
Nikita <nikmoon16@gmail.com>
2025-03-02 03:37:36 +0100

    Initial commit

---
+++ src/main/java/hgc/autorole/Autorole.java
@@ -0,0 +1,17 @@
+package hgc.autorole;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Autorole extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
